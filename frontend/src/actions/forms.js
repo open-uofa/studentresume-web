e9056758d9ce73b06b36a70c2e1e8c22fc6e93c3
IvanZyf666 <yzhang24@ualberta.ca>
2022-12-03 10:34:30 -0700

    changed api address back

--- frontend/src/actions/forms.js
+++ frontend/src/actions/forms.js
@@ -5,8 +5,8 @@ export const submitForm = (data, themeNum) => {
     return new Promise((resolve, reject) => {
         // const json_data = JSON.stringify(data);
         // console.log("sending data: ", json_data);
-        axios.post("http://localhost:8000/resume?theme=" + themeNum, data, {
-        // axios.post('http://[2605:fd00:4:1001:f816:3eff:fe21:fc]/resume?theme=' + themeNum, data, {
+        // axios.post("http://localhost:8000/resume?theme=" + themeNum, data, {
+        axios.post('http://[2605:fd00:4:1001:f816:3eff:fe21:fc]/resume?theme=' + themeNum, data, {
             headers: {
                 'Content-Type': 'application/json'
             }
@@ -34,8 +34,8 @@ export const validateForm = (json_data) => {
     // send form data to backend
     return new Promise((resolve, reject) => {
 
-        axios.post("http://localhost:8000/validate", json_data, {
-        // axios.post('http://[2605:fd00:4:1001:f816:3eff:fe21:fc]/validate', json_data, {
+        // axios.post("http://localhost:8000/validate", json_data, {
+        axios.post('http://[2605:fd00:4:1001:f816:3eff:fe21:fc]/validate', json_data, {
             headers: {
                 'Content-Type': 'application/json'
             }
